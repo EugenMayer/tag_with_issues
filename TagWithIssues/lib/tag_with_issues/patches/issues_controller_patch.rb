f37acbb5dac35e8455182feb337b3b1dd5a6e82f
Florian Pommerening <admin@praedisoft.de>
2012-03-06 11:03:28 +0100

    refs #743: list current tags in view

--- TagWithIssues/lib/tag_with_issues/patches/issues_controller_patch.rb
+++ TagWithIssues/lib/tag_with_issues/patches/issues_controller_patch.rb
@@ -9,7 +9,10 @@ module TagWithIssues
           # add the tag action to the before_filter methods that trigger find_issues
           # otherwise the @issues variable is not generated from the ids in params.
           # In particular @project will not be set, causing authorize to fail
-          base.filter_chain.detect{|filter| filter.method == :find_issues}.options[:only] << "tag"
+          base.filter_chain.detect{|filter| filter.method == :find_issues}.options[:only] << "tag" << "create_tag"
+          
+          before_filter :check_unique_project, :only => [:tag, :create_tag]
+          before_filter :find_repository, :only => [:tag, :create_tag]
         end
       end
 
@@ -17,6 +20,26 @@ module TagWithIssues
         def tag
           @issues.sort!
           @major_version = Setting.plugin_tag_with_issues['major_version']
+          has_branches = (!@repository.branches.nil? && @repository.branches.length > 0)
+        end
+        
+        def create_tag
+        end
+        
+        private
+
+        def check_unique_project
+          unless @project
+            render_error 'Tagging a commit with issues from other projects is not supported'
+            return false
+          end
+        end
+        
+        def find_repository
+          @repository = @project.repository
+          (render_404; return false) unless @repository
+        rescue ActiveRecord::RecordNotFound
+          render_404
         end
       end
     end
